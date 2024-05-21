f35679ef9417bcb339fa6c2313b28cd605bb46f1
Huong Nguyen <huonghao9320@gmail.com>
2024-05-21 17:57:26 +0700

    huong nguyen

---
+++ HtmlDom/dom.js
@@ -0,0 +1,31 @@
+function changeText() {
+  var paragraph = document.getElementById("myParagraph");
+  paragraph.innerText =
+    "<span>Hello <span style='display: none;'>World</span></span>";
+
+  console.log(paragraph);
+}
+
+function changeColor() {
+  var color = document.getElementById("myDiv");
+  color.style.backgroundColor = "red";
+  console.log(color);
+}
+
+const list = document.getElementById("myList");
+function addItem() {
+  const newItem = document.createElement("li");
+  newItem.textContent =
+    "<span>Hello <span style='display: none;'>World</span></span>";
+  list.appendChild(newItem);
+}
+
+function removeItem() {
+  list.removeChild(list.lastChild);
+}
+
+function displayValue() {
+  const inputValue = document.getElementById("myInput").value;
+  const displayArea = document.getElementById("displayArea");
+  displayArea.textContent = inputValue;
+}
